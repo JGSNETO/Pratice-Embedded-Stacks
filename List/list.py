08d0b11dfc3e9f7ae332d3c146cc953ad5f10374
José Neto <jgsnto.sd@gmail.com>
2022-10-12 16:57:14 -0300

    List

---
+++ List/list.py
@@ -0,0 +1,10 @@
+"""
+Python list are just like dynamically sized arrays, declare in other languages. In simple language, a list is a 
+collection of things, enclosed in [] and separeted by commas.
+"""
+
+List = [1,2,3,4,5,5,5]
+List.remove(5)
+print(List)
+List.append(10)
+print(List)
